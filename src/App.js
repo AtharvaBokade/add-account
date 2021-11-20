f507cf6ca9b7df4ebc58121f2ef0c441e9e0c359
Atharva <cortom953@gmail.com>
2021-11-20 23:25:45 +0530

    MongoDB added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://cortom-add-account:456Pass1234@cluster0.emmrp.mongodb.net/Accounts?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  const doc = {
+    Name : "Atharva";
+    Mobile_no: "7020084093";
+  }
+  db.collection("devices").insertOne(doc);
+  client.close();
+});
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
   );
 }
 
